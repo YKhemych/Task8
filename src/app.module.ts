37a5c454e79ead60d73d3a16d231b606326de668
YKhemych <yosyp0108khemych@gmail.com>
2019-08-27 15:05:13 +0300

    Create messages module, service, controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConversationsModule } from './conversations/conversations.module';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [UsersModule, ConversationsModule],
+  imports: [UsersModule, ConversationsModule, MessagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
