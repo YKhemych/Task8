c4635d865b497c417f30ae2969d2e21247920b32
YKhemych <yosyp0108khemych@gmail.com>
2019-08-27 15:46:46 +0300

    Add relation one to many for User and Messages

--- src/messages/message.entity.ts
+++ src/messages/message.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { User } from '../users/user.entity';
 
 @Entity()
 export class Message {
@@ -10,4 +11,7 @@ export class Message {
 
   @Column({ length: 1000})
   text: string;
+
+  @ManyToOne(type => User, user => user.messages)
+  user: User;
 }
